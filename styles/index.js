705b8b76b8110394e1c544304f486df1f4e50283
shoycodes <83496780+shoycodes@users.noreply.github.com>
2021-05-03 14:55:10 +0200

    added weather app project folder to created repository

---
+++ styles/index.js
@@ -0,0 +1,95 @@
+// Search city, current location, weather description
+function searchPlace(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-input");
+  let cityInput = document.querySelector(".form-control");
+  cityElement.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
+}
+
+let searchForm = document.querySelector(".search");
+searchForm.addEventListener("submit", searchPlace);
+
+function displayWeatherCondition(response) {
+  let cityElement = document.querySelector("#city-input");
+  let description = document.querySelector("#temp-description");
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#degrees");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = `${temperature}`;
+  description.innerHTML = response.data.weather[0].description;
+}
+
+function searchCity(city) {
+  let units = "imperial";
+  let apiKey = "318f9dfeb43dee4969a3fd201bd62795";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "imperial";
+  let apiKey = "318f9dfeb43dee4969a3fd201bd62795";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
+
+//
+// Return current time
+
+let now = new Date();
+
+let day = now.getDay();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let currentDay = days[now.getDay()];
+let dateTime = document.querySelector("#date");
+dateTime.innerHTML = `${currentDay} ${hours}:${minutes}`;
+
+// Swap between C and F
+
+function changeToFahrenheit(event) {
+  event.preventDefault();
+  let degrees = document.querySelector("#degrees");
+  degrees.innerHTML = Math.round((degrees.innerHTML * 9) / 5 + 32);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", changeToFahrenheit);
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#degrees");
+  temperatureElement.innerHTML = Math.round(
+    ((temperatureElement.innerHTML - 32) * 5) / 9
+  );
+}
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", convertToCelsius);
