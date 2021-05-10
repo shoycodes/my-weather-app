3996dfc548bbbeff83d4b620f84469d7c163dc4a
shoycodes <83496780+shoycodes@users.noreply.github.com>
2021-05-10 23:26:10 +0200

    fixed the js image reference and body fonts

--- styles/index.js
+++ styles/index.js
@@ -25,7 +25,7 @@ let currentDay = days[now.getDay()];
 let dateTime = document.querySelector("#date");
 dateTime.innerHTML = `${currentDay} ${hours}:${minutes}`;
 
-//background change:
+//background-image change depending on time:
 setInterval(change_background, 1000 * 60 * 2);
 
 function change_background(){
@@ -33,12 +33,13 @@ let d = new Date();
 let n = d.getHours();
 console.log(n);
 if (n == 22 || n < 7) {
-  document.body.style.backgroundImage = "url(https://images.pexels.com/photos/2341830/pexels-photo-2341830.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)";
-} else if (n == 7 || n <13) {
   document.body.style.backgroundImage = "url(https://images.pexels.com/photos/531767/pexels-photo-531767.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)";
+} else 
+if (n == 7 || n <13) {
+  document.body.style.backgroundImage = "url(https://images.pexels.com/photos/1673973/pexels-photo-1673973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)";
 }
 else {
-  document.body.style.backgroundImage = "url(https://images.pexels.com/photos/1673973/pexels-photo-1673973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)";
+  document.body.style.backgroundImage = "url(https://images.pexels.com/photos/2341830/pexels-photo-2341830.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)";
 }
 }
 
